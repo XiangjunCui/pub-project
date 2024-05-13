982c4b4b44c4a6203f822f649b0c179003cb782e
Xiangjun Cui <40485486+XiangjunCui@users.noreply.github.com>
2024-05-13 01:03:37 -0500

    Add files via upload

---
+++ Magazine.java
@@ -0,0 +1,21 @@
+//Xiangjun Cui
+
+package publicationproject;
+
+
+public class Magazine extends Publication {
+    
+    private String publisher;
+    
+    public Magazine(String Publisher, String Title, Genre Genre, int Edition, int Year) {
+        super(Title, Genre, Edition, Year);
+        this.publisher = Publisher;
+    } 
+    @Override
+    public void printInfo()
+    {
+        super.printInfo();
+        System.out.println(genre.name() + " magazine by " + publisher);
+        super.printFooter();
+    }
+}
